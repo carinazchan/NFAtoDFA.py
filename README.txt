635d791eb0b44e6f98a5ce7cb23e4188b3659e10
Carina Chan <carinazchan@gmail.com>
2024-03-05 16:40:29 -0800

    Initial commit, ReadWrite class created
    - __init__ function works.
    - CheckCommandLine function works.
    - WriteToOutput function works.
    - Only tested with given input file.

---
+++ README.txt
@@ -0,0 +1,6 @@
+README
+
+Instructions:
+- Make sure all necessary .py files are in one folder.
+- In Terminal, cd to the correct directory (the folder containing all your .py files for this project).
+- Run in the terminal: pytest
---
+++ ReadWrite.py
@@ -0,0 +1,59 @@
+import sys
+
+'''
+Reads from an input file and writes to an output file.
+'''
+class ReadWrite:
+    def __init__(self, inputFilePath, outputFilePath):
+        self.inputFilePath = inputFilePath
+        self.outputFilePath = outputFilePath
+
+    def CheckCommandLine(self):
+        #sys.argv: A list of command-line arguments passed to a Python script. Script name is the first element (sys.argv[0])
+        if len(sys.argv) != 2: #If the length of the command line arguments is != 2
+            print("Need two arguments: YourScript.py InputFile.txt")
+        sys.exit(1) #Exit the program
+
+    # def SetInputFilePath(self):
+    #     self.inputFilePath = sys.argv[1] #Second argument is the file path
+
+    # def GetLine(self):
+    #     with open(self.inputFilePath, 'r') as file: #Open the file in read mode ('r')
+    #         #Iterate through each line in the file
+    #         lines = file.read().split('\n')
+    #         # Read Q: lines[0]
+    #         Q = lines[0].replace('{', '').replace('}', '').split('\t')
+    #         print("this is Q")
+    #         print(Q)
+    #         # Read Sigma: lines[1]
+    #         sigma = lines[1].split('\t')
+    #         print(sigma)
+    #         # Read q0: lines[2]
+    #         q0 = lines[2].replace('{', '').replace('}', '').split('\t')
+    #         print(q0)
+    #         # Read F: lines[3]
+    #         F = lines[3].replace('{', '').replace('}', '').split('\t')
+    #         print(F)
+    #         # Read transitions: lines[4:]
+    #         delta = []
+    #         for line in lines[4:]:
+    #             if (line == "BEGIN" or line == ""):
+    #                 continue
+    #             elif (line == "END"):
+    #                 break
+    #             delta.append(line)
+    #         print(delta)
+    #             #print(line.strip())  # Example: Print each line after removing leading and trailing whitespaces
+    #     return (Q, sigma, q0, F, delta)
+
+
+    def WriteToOutput(self, outputFilePath):
+        # Open the file in write mode
+        with open(outputFilePath, 'w') as file:
+            # Write content to the file
+            file.write("Hello, this is a sample text.\n")
+            file.write("Writing to a text file in Python is easy!")
+
+        file.close()
+
+        print(f"File '{outputFilePath}' has been created and written to.")
