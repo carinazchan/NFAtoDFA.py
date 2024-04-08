63d0e89d313a2db8f9c29fc47fa704105deb5628
Carina Chan <carinazchan@gmail.com>
2024-04-07 21:15:45 -0700

    F

--- test_ReadWrite.py
+++ test_ReadWrite.py
@@ -8,29 +8,29 @@ from unittest.mock import patch
 def test___init__():
     with patch.object(sys, "argv", ["test.py", "argument1"]):
         # Create an instance of ReadWrite.
-        ReadWriteInstance = ReadWrite.ReadWrite(["script", "test"])
+        readWriteInstance = ReadWrite.ReadWrite(["script", "test"])
 
         # Check if the inputFilePath is taken from the command line arguments.
-        assert ReadWriteInstance.inputFilePath == "test"
+        assert readWriteInstance.inputFilePath == "test"
 
 
 def test_CheckCommandLine():
     # Test if the CheckCommandLine method returns None when the correct amount of arguments are passed.
     with patch.object(sys, "argv", ["test.py", "argument1"]):
         # Create an instance of ReadWrite.
-        ReadWriteInstance = ReadWrite.ReadWrite(sys.argv)
+        readWriteInstance = ReadWrite.ReadWrite(sys.argv)
 
         # Check if the CheckCommandLine method returns None.
-        assert ReadWriteInstance.CheckCommandLine() == None
+        assert readWriteInstance.CheckCommandLine() == None
 
     # Test if the CheckCommandLine method returns an error message when the incorrect amount of arguments are passed.
     with patch.object(sys, "argv", ["test.py", "argument1", "argument2"]):
         # Create an instance of ReadWrite.
-        ReadWriteInstance = ReadWrite.ReadWrite(sys.argv)
+        readWriteInstance = ReadWrite.ReadWrite(sys.argv)
 
         # Check if the CheckCommandLine method returns None.
         assert (
-            ReadWriteInstance.CheckCommandLine()
+            readWriteInstance.CheckCommandLine()
             == "Need two arguments: YourScript.py InputFile.txt"
         )
 
@@ -52,14 +52,11 @@ def test_GetLine():
     END
     """
 
-    # Create a mock file object.
-    mockFile = io.StringIO(mockFileContents)
-
     # Create an instance of ReadWrite.
-    ReadWriteInstance = ReadWrite.ReadWrite(sys.argv)
+    readWriteInstance = ReadWrite.ReadWrite(sys.argv)
 
     # Check if the GetLine method returns the correct values.
-    assert ReadWriteInstance.GetLine(mockFile) == (
+    assert readWriteInstance.GetLine(mockFileContents) == (
         ["1", "2", "3"]["a", "b"]["1"]["1"][
             "{1}, EPS = {3}",
             "{1}, b = {2}",
